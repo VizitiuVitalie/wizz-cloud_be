81a193a8cd62b45f1714032da4989b9cc69f2a02
Roman Vizitiu <md.vizitiuroman@gmail.com>
2024-08-27 13:27:22 +0300

    fix modules

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,22 +3,21 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UserDto } from './dto/user.dto';
 import { UserServiceInterface } from './interfaces/user.service.interface';
 import { UserAdapterInterface } from './interfaces/user.adapter.interface';
+import { UserAdapter } from './user.adapter';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(
-    @Inject('UserServiceInterface') private readonly userService: UserServiceInterface,
-    @Inject('UserAdapterInterface') private readonly userAdapter: UserAdapterInterface,
+    @Inject(UserService) private readonly userService: UserServiceInterface,
+    @Inject(UserAdapter) private readonly userAdapter: UserAdapterInterface,
   ) {}
 
   @Post('create')
   public async create(@Body() dto: CreateUserDto): Promise<UserDto> {
     const domain = this.userAdapter.FromCreateUserDtoToDomain(dto);
-
     const createdDomain = await this.userService.create(domain);
 
-    const createdDto = this.userAdapter.FromDomainToDto(createdDomain);
-
-    return createdDto
+    return this.userAdapter.FromDomainToDto(createdDomain);
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { UserServiceInterface } from './interfaces/user.service.interface';
 import { UserDomain } from './domain/user.domain';
 import { UserRepo } from './user.repo';
@@ -8,10 +8,11 @@ import { UserEntity } from './domain/user.entity';
 @Injectable()
 export class UserService implements UserServiceInterface {
   public constructor(
+    @Inject(UserRepo)
     private readonly userRepo: UserRepoInterface<UserDomain, UserEntity>,
   ) {}
 
   public create(domain: UserDomain): Promise<UserDomain> {
-      return this.userRepo.save(domain)
+    return this.userRepo.save(domain);
   }
 }
