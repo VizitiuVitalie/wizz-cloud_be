3f543d31e0aedceb3f43612e861a9a6960b7543d
Roman Vizitiu <md.vizitiuroman@gmail.com>
2024-08-27 13:27:22 +0300

    fix modules

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,1 +1,14 @@
 import { Module } from '@nestjs/common';
+import { DbModule } from '../../core/db/db.module';
+import { UserAdapter } from './user.adapter';
+import { UserController } from './user.controller';
+import { UserRepo } from './user.repo';
+import { UserService } from './user.service';
+
+@Module({
+  imports: [DbModule],
+  controllers: [UserController],
+  providers: [UserRepo, UserService, UserAdapter],
+  exports: [UserService],
+})
+export class UserModule {}
