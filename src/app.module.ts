3a0b21eb82b49030c0605846976abfb703026fbf
vvitaliy <vizitiuvitalie.dev@gmail.com>
2024-10-14 01:30:02 +0300

    local-storage integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { Config } from './core/config/config';
 import { UserModule } from './modules/user/user.module';
+import { ContentModule } from './modules/content/content.module';
+import { StorageModule } from './modules/storage/storage.module';
 
 @Module({
   imports: [
@@ -9,6 +11,8 @@ import { UserModule } from './modules/user/user.module';
       load: [Config],
     }),
     UserModule,
+    ContentModule,
+    StorageModule,
   ],
 })
 export class AppModule {}
