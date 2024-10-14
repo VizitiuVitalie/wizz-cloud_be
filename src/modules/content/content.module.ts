3243113feeb245cb534ac6936b93ebc81f958aaa
vvitaliy <vizitiuvitalie.dev@gmail.com>
2024-10-14 12:23:37 +0300

    fixed content.module.ts

--- src/modules/content/content.module.ts
+++ src/modules/content/content.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ContentController } from './content.controller';
-import { StorageModule } from '../storage/storage.module';
+import { ContentService } from './content.service'; // Импортируем ContentService
+import { ContentAdapter } from './content.adapter'; // Импортируем ContentAdapter
+import { StorageModule } from '../storage/storage.module'; // Импорт StorageModule
+import { ContentRepo } from './content.repo';
+import { DbModule } from 'src/core/db/db.module';
 
 @Module({
-  imports: [StorageModule],
-  controllers: [ContentController],
+  imports: [DbModule, StorageModule], // Импорт StorageModule для работы с файлами
+  controllers: [ContentController], // Регистрация контроллера
+  providers: [ContentRepo, ContentService, ContentAdapter],
+  exports: [ContentService]
 })
 export class ContentModule {}
