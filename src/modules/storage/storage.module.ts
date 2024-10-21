d7653c203c3f5af81e24ee3f6eb539110b6d13f0
vvitaliy <vizitiuvitalie.dev@gmail.com>
2024-10-21 23:53:52 +0300

    deleted useless directories and fixed storage.module

--- src/modules/storage/storage.module.ts
+++ src/modules/storage/storage.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
-import { LocalStorage } from './local-storage.service'; // Локальная реализация хранилища
-import { MulterModule } from '@nestjs/platform-express';
-import { multerConfig } from 'src/core/config/multer.config'; // Конфигурация Multer
+import { LocalStorage } from './local-storage.service';
 
 @Module({
-  imports: [
-    MulterModule.register(multerConfig), // Регистрация конфигурации для Multer
-  ],
-  providers: [LocalStorage], // Локальная служба для хранения файлов
-  exports: [LocalStorage], // Экспорт для использования в других модулях
+  providers: [LocalStorage],
+  exports: [LocalStorage],
 })
 export class StorageModule {}
