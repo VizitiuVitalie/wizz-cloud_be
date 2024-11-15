5983d48ae0d7a72b2e4acd45fbeec646461d7cc2
VizitiuVitalie <vizitiuvitalie.dev@gmail.com>
2024-11-15 21:26:54 +0200

    fixed bugs with strategy

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,22 +3,33 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt/jwt.strategy';
+import { AuthController } from './auth.controller';
+import { UserModule } from '../user/user.module';
+import { DbModule } from 'src/core/db/db.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Config } from 'src/core/config/config';
 
 @Module({
   imports: [
-    ConfigModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [Config],
+    }),
+    DbModule,
+    UserModule,
     PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('jwt.secret'),
+        secret: configService.get<string>('JWT_SECRET'),
         signOptions: { expiresIn: '60m' },
       }),
       inject: [ConfigService],
     }),
+    ConfigModule,
   ],
   providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
   exports: [AuthService],
 })
 export class AuthModule {}
