278d03b134b9ed96f3e396b4e957e8e0676c0d56
VizitiuVitalie <vizitiuvitalie.dev@gmail.com>
2024-11-28 23:17:14 +0200

    added cors to get requests from client

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigEnums } from './core/config/config.enums';
 import { HttpConfig } from './core/config/http.config';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  };
+  app.enableCors(corsOptions);
 
   const httpConfig = app.get(ConfigService).get<HttpConfig>(ConfigEnums.HTTP);
 
