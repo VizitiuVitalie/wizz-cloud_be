b9ab5dbf7e56ae34758b073cdb93af3c044f9308
VizitiuVitalie <vizitiuvitalie.dev@gmail.com>
2024-12-01 01:30:39 +0200

    added getByUserId method at content module

--- src/modules/content/interfaces/content.repo.interface.ts
+++ src/modules/content/interfaces/content.repo.interface.ts
@@ -3,6 +3,7 @@ import { AbstractEntity } from '../../../shared/entities/abstract.entity';
 export interface ContentRepoInterface<D, E extends AbstractEntity> {
   save(domain: D): Promise<D>;
   findById(id: number): Promise<D | null>;
+  findByUserId(userId: number): Promise<D[]>;
   update(domain: D): Promise<D>;
   deleteById(id: number): Promise<void>;
 }
