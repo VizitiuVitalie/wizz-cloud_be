12e6290c2e42893dbf4afbf355eba8e0b796cd21
VizitiuVitalie <vizitiuvitalie.dev@gmail.com>
2024-12-04 16:42:34 +0200

    structure improvements

--- src/libs/local-storage/local-storage.service.ts
+++ src/libs/local-storage/local-storage.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { promises as fs } from 'fs';
 import * as path from 'path';
-import { ContentStorageI } from './interfaces/content-storage.interface';
+import { LocalStorageI } from './interfaces/local-storage.interface';
 import { v4 as uuidv4 } from 'uuid';
 
 
 @Injectable()
-export class LocalStorage implements ContentStorageI {
+export class LocalStorage implements LocalStorageI {
     public async save(file: Express.Multer.File, destination: string): Promise<string> {
         const filePath = path.join(destination, `${file.originalname}-${uuidv4()}`);
         await fs.writeFile(filePath, file.buffer)
