f13764b5102ab1fec9e9663f48b67ad159dbb1fa
VizitiuVitalie <vizitiuvitalie.dev@gmail.com>
2024-12-06 17:44:25 +0200

    fixed routes for getLocalUserContents and getBucketContentById because of route conflict

--- src/modules/content/interfaces/content.service.interface.ts
+++ src/modules/content/interfaces/content.service.interface.ts
@@ -4,7 +4,7 @@ import { ContentDomain } from '../domain/content.domain';
 export interface ContentServiceInterface {
   uploadContent(domain: ContentDomain, file: Express.Multer.File): Promise<ContentDomain>;
   findById(id: number): Promise<ContentDomain | null>;
-  findByUserId(id: number): Promise<ContentDomain[]>;
+  findByUserId(userId: number): Promise<ContentDomain[]>;
   getFileStream(fileUrl: string): Promise<Readable>
   update(domain: ContentDomain): Promise<ContentDomain>;
   deleteById(id: number): Promise<void>;
