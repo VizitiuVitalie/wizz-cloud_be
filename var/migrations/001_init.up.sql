b3b97c18dbf90bfc4c0cc5306c26ea13b80e5a13
VizitiuVitalie <vizitiuvitalie.dev@gmail.com>
2024-12-10 19:06:09 +0200

    fixed dockerfile with nodemon

--- var/migrations/001_init.up.sql
+++ var/migrations/001_init.up.sql
@@ -3,8 +3,6 @@ CREATE TABLE users (
   full_name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
-  verified BOOLEAN DEFAULT FALSE,
-  verification_code VARCHAR(6),
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
